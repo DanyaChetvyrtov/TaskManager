aa8f34ae44a465af3d372004c04ac325ce99efd9
Danil <dabcom2013@gmail.com>
2025-06-19 18:39:16 +0300

    account-ms. Update account. Add fields [created/updated]At

--- services/account-ms/src/main/java/ru/zuzex/practice/accountms/model/Account.java
+++ services/account-ms/src/main/java/ru/zuzex/practice/accountms/model/Account.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
@@ -15,13 +16,31 @@ import java.util.UUID;
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "id")
+    @Column(name = "id", nullable = false)
     private UUID accountId;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String name;
-    @Column(name = "surname")
+
+    @Column(name = "surname", nullable = false)
     private String surname;
-    @Column(name = "age")
+
+    @Column(name = "age", nullable = false)
     private Integer age;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
--- services/account-ms/src/main/resources/db/changelog/changesets/V1__create_table.sql
+++ services/account-ms/src/main/resources/db/changelog/changesets/V1__create_table.sql
@@ -2,9 +2,11 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto; -- эт нужно для gen_random_uu
 
 CREATE TABLE account
 (
-    id      uuid DEFAULT gen_random_uuid() PRIMARY KEY,
-    name    varchar(100)                           NOT NULL,
-    surname varchar(100)                           NOT NULL,
-    age     integer check (age BETWEEN 0 AND 100 ) NOT NULL
+    id         uuid      DEFAULT gen_random_uuid() PRIMARY KEY,
+    name       varchar(100)                           NOT NULL,
+    surname    varchar(100)                           NOT NULL,
+    age        integer CHECK (age BETWEEN 0 AND 100 ) NOT NULL,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
