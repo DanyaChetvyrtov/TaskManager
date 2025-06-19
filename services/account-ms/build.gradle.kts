7d544593f347ce27aca02cd261456869235467d5
Danil <dabcom2013@gmail.com>
2025-06-19 21:55:23 +0300

    configurations. Test vault connection

--- services/account-ms/build.gradle.kts
+++ services/account-ms/build.gradle.kts
@@ -29,9 +29,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.cloud:spring-cloud-starter-vault-config")
 
 	// db
 	runtimeOnly("org.postgresql:postgresql")
