0d133a2437a97d18841229580141fc5ecc091419
Danil <dabcom2013@gmail.com>
2025-07-04 19:53:48 +0300

    sh. Refactor vault-initializer

--- vault-initializer.sh
+++ vault-initializer.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/sh
 set -e
 
 # Настраиваем переменные окружения
@@ -7,31 +6,35 @@ export VAULT_ADDR="http://$VAULT_DEV_LISTEN_ADDRESS"
 export VAULT_TOKEN="$VAULT_DEV_ROOT_TOKEN_ID"
 
 # Включаем KV v2 secrets engine если ещё не включен
-echo "Настройка Vault..."
-vault secrets enable -path=secret kv-v2 || echo "Secrets engine already started"
+#echo "Настройка Vault..."
+#vault secrets enable -path=secret kv-v2 || echo "Secrets engine already started"
+
+SECRET_KEY_VALUE=aXQncyBzaW1wbGUgc2VjcmV0IGZvciBzdW1tZXIgcHJhY3RpY2UgWlVaRVggcHJvamVjdC4gVGFza01hbmFnZXI=
+DB_USERNAME_VALUE=postgres
+DB_PASSWORD_VALUE=root
 
 # Создаём секреты
 vault kv put secret/kv/auth-ms \
-  DB_USERNAME="postgres" \
-  DB_PASSWORD="root" \
-  SECRET_KEY="aXQncyBzaW1wbGUgc2VjcmV0IGZvciBzdW1tZXIgcHJhY3RpY2UgWlVaRVggcHJvamVjdC4gVGFza01hbmFnZXI=" \
+  DB_USERNAME="$DB_USERNAME_VALUE" \
+  DB_PASSWORD="$DB_PASSWORD_VALUE" \
+  SECRET_KEY="$SECRET_KEY_VALUE" \
   ACCESS_DURATION=15 \
   REFRESH_DURATION=7
 
 vault kv put secret/kv/account-ms \
-  DB_USERNAME="postgres" \
-  DB_PASSWORD="root" \
-  SECRET_KEY="aXQncyBzaW1wbGUgc2VjcmV0IGZvciBzdW1tZXIgcHJhY3RpY2UgWlVaRVggcHJvamVjdC4gVGFza01hbmFnZXI="
+  DB_USERNAME="$DB_USERNAME_VALUE" \
+  DB_PASSWORD="$DB_PASSWORD_VALUE" \
+  SECRET_KEY="$SECRET_KEY_VALUE"
 
 vault kv put secret/kv/task-ms \
-  DB_USERNAME="postgres" \
-  DB_PASSWORD="root" \
-  SECRET_KEY="aXQncyBzaW1wbGUgc2VjcmV0IGZvciBzdW1tZXIgcHJhY3RpY2UgWlVaRVggcHJvamVjdC4gVGFza01hbmFnZXI="
+  DB_USERNAME="$DB_USERNAME_VALUE" \
+  DB_PASSWORD="$DB_PASSWORD_VALUE" \
+  SECRET_KEY="$SECRET_KEY_VALUE"
 
 # Выводим информацию о созданных секретах
 echo "Секреты успешно созданы:"
 vault kv list secret
 
 # Бесконечное ожидание чтобы контейнер не завершал работу
-echo "Vault готов к использованию"
-tail -f /dev/null
+#echo "Vault готов к использованию"
+#tail -f /dev/null
